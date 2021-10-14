1b8d2450237a5b2a809cb53197140005c859cc59
Dave Beidle <dbeidle@gmail.com>
2021-10-14 11:29:31 -0500

    removed extra console.log statement

--- react/src/index.js
+++ react/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 
 const App = () => {
   console.log("I'm sorry Dave. I'm afraid I can't do that...");
-  console.log("Oh yea, well screw you! sudo halt --now");
   return (
     <div>
       <h3>Webpack Template for React!</h3>
