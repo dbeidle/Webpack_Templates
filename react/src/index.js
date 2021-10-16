5f9b297593bfece2b770afcee3b8edeaec373946
Dave Beidle <dbeidle@gmail.com>
2021-10-16 06:18:55 -0500

    A little addition to the index.js/css files for a little fun

--- react/src/index.js
+++ react/src/index.js
@@ -4,10 +4,18 @@ import "./index.css";
 
 const App = () => {
   console.log("I'm sorry Dave. I'm afraid I can't do that...");
+  console.log("Oh yea, well screw you! sudo halt --now");
   return (
     <div>
       <h3>Webpack Template for React!</h3>
-      <img src="images/icon.svg" alt="Webpack Logo" />;
+      <div>
+        <img className="wp-logo" src="images/icon.svg" alt="Webpack Logo" />
+        <img
+          className="react-logo"
+          src="images/react-logo.svg"
+          alt="React Logo"
+        />
+      </div>
     </div>
   );
 };
